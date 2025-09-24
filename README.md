1b4344d3b737b89fc4f9aab8bd39586256080976
SuikaRyp <ciulbotak25@gmail.com>
2025-09-24 17:04:45 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+#Script Killua-MD
+*Base By Kyami Silence*
+*Pengembang Script Suika And Yaanz*
+*Baileys By Kiuur*
+
+#Script ini gratis Dari Suika And Yaanz
+#Dilarang Menjual Belikan
+
+#Sumber
+#https://whatsapp.com/channel/0029Vb6MElML7UVYzUZmP21R
---
+++ config.js
@@ -0,0 +1,110 @@
+require("./Zion")
+const fs = require('fs')
+const { version } = require("./package.json")
+//~~~~~~~~~SETTING BOT~~~~~~~~~~//
+
+// Bebas Ubah
+global.owner = "6285187979614"
+global.nobot = "62XXXX"
+global.namaowner = "Suika"
+global.namaBot = "Killua-MD"
+global.title = "Killua-MD"
+
+// Jangan Di ubah
+global.creator = `${owner}@s.whatsapp.net` 
+global.foother = `© ${namaBot}`
+global.versi = version
+global.nama = namaBot 
+global.namach = nama 
+global.namafile = foother 
+global.author = namaowner
+
+// Bebas Ubah
+// True = on || False = Off 
+global.autoread = false
+global.autotyping = false
+global.Antilinkgc = false
+global.Antilinkch = false
+global.antispam = false
+global.onlygc = false
+global.autobio = false
+
+// Set Payment
+global.qris = "https://files.catbox.moe/8h0v3n.jpg"
+global.dana = "08512345678"
+global.gopay = "08512345678"
+
+// ===={ Set Link }
+global.ch = 'https://whatsapp.com/channel/0029Vb6MElML7UVYzUZmP21R'
+global.idch = '120363417530275169@newsletter'
+global.linkgc = ''
+global.yt = 'https://youtube.com/@fanstimetoon'
+global.nekorin = "https://api.nekorinn.my.id"
+global.idgc = ""
+// set prefix
+global.setprefix = "", "/", "#"
+
+// User Sosmed
+global.tt = "@yforyaanz"
+global.yt = "@fanstimetoon"
+global.ig = "@suikaryp"
+
+// Setting Api cVPS
+global.doToken = "APIKEY"
+global.linodeToken = "APIKEY"
+
+// Settings Api Panel Pterodactyl
+global.egg = "15" // Egg ID
+global.nestid = "5" // nest ID
+global.loc = "1" // Location ID
+global.domain = "https://"
+global.apikey = "ptla" //ptla
+global.capikey = "ptlc" //ptlc
+
+// [ THEME URL & URL ] ========//
+global.thumbnail = 'https://img1.pixhost.to/images/8907/643554251_zion.jpg'
+
+// Settings reply ~~~~~~~~~//
+global.mess = {
+    owner: "Khusus Owner",
+    prem: "Khusus Premium",
+    group: "Khusus di Group Chat",
+    admin: "Khusus Admin",
+    botadmin: "Bot Harus Jadi Admin",
+    private: "Khusus di Private Chat",
+    done: "Sukses"
+}
+
+global.packname = nama
+global.author = namaBot
+
+//
+global.gamewaktu = 60 // Game waktu
+global.suit = {};
+global.tictactoe = {};
+global.petakbom = {};
+global.kuis = {};
+global.siapakahaku = {};
+global.asahotak = {};
+global.susunkata = {};
+global.caklontong = {};
+global.family100 = {};
+global.tebaklirik = {};
+global.tebaklagu = {};
+global.tebakgambar2 = {};
+global.tebakkimia = {};
+global.tebakkata = {};
+global.tebakkalimat = {};
+global.tebakbendera = {};
+global.tebakanime = {};
+global.kuismath = {};
+
+//~~~~~~~~~~~ DIEMIN ~~~~~~~~~~//
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
